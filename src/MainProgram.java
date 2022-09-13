7d0f795b0044ad382de42eb8e52bd092d1b6c13d
mrdtbelo <belomaridiat@gmail.com>
2022-09-13 11:19:40 +0200

    jdfkvkubv

--- src/MainProgram.java
+++ src/MainProgram.java
@@ -5,7 +5,7 @@ public class MainProgram {
         System.out.println(e1.getName()+"-"+e1.getFirstname()+"-"+e1.getBirthdate()+"-"+e1.getGender()+"-"+e1.getAge()
                             +"-"+e1.getWeight()+"-"+e1.getDepartment()+"-"+e1.getSalary()+"-"+e1.getYearsOfService()+"-"+e1.getParticularity());
 
-        Employee e2 = new Employee("0", "0", "0", 'm', 0, 0.0, "0" );
+        Employee e2 = new Employee("Stefanie", "Krug", "09.06.2000", 'f', 22, 0.0, "0" );
         System.out.println(e2.getName()+"-"+e2.getFirstname()+"-"+e2.getBirthdate()+"-"+e2.getGender()+"-"+e2.getAge()
                             +"-"+e2.getWeight()+"-"+e2.getDepartment()+"-"+e2.getSalary()+"-"+e2.getYearsOfService()+"-"+e2.getParticularity());
     }
