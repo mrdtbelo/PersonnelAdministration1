272f65ceec3d24eea13955199c6a6ea93ae4fb10
mrdt.07 <belomaridiat@gmail.com>
2022-09-19 17:36:04 +0200

    dcvbfgfgcff

--- src/Employee.java
+++ src/Employee.java
@@ -11,27 +11,37 @@ public class Employee {
     private int yearsOfService;
     private String particularity;
 
-    public Employee(String n, String f, String b, char g, int a, double w, String p) {
+    public Employee(String n, String f, String b, char g, int a, double w, String d, String p) {
        name = n;
        firstname = f;
        birthdate =  b;
        gender = g;
        age = a;
        weight = w;
-       department = "A38";
+       department = d;
        salary = 1500;
        yearsOfService = 0;
        particularity = p;
     }
 
-    public String getName(){ return name; }
-    public String getFirstname(){ return firstname; }
-    public String getBirthdate(){ return birthdate; }
-    public char getGender(){ return gender; }
-    public int getAge(){ return age; }
-    public double getWeight(){ return weight; }
-    public String getDepartment(){ return department; }
-    public double getSalary(){ return salary;}
-    public int getYearsOfService(){ return yearsOfService; }
-    public String getParticularity(){ return particularity; }
+    public String getName(){
+        return name; }
+    public String getFirstname(){
+        return firstname; }
+    public String getBirthdate(){
+        return birthdate; }
+    public char getGender(){
+        return gender; }
+    public int getAge(){
+        return age; }
+    public double getWeight(){
+        return weight; }
+    public String getDepartment(){
+        return department; }
+    public double getSalary(){
+        return salary;}
+    public int getYearsOfService(){
+        return yearsOfService; }
+    public String getParticularity(){
+        return particularity; }
 }
--- src/MainProgram.java
+++ src/MainProgram.java
@@ -1,11 +1,11 @@
 public class MainProgram {
     public static void main(String[] args){
 
-        Employee e1 = new Employee( "Marco", "Neumann", "23.10.1971", 'm', 0, 88.5, "0" );
+        Employee e1 = new Employee( "Neumann", "Marco", "23.10.1971", 'm', 50, 88.9, " C12", "works mondays to fridays" );
         System.out.println(e1.getName()+"-"+e1.getFirstname()+"-"+e1.getBirthdate()+"-"+e1.getGender()+"-"+e1.getAge()
-                            +"-"+e1.getWeight()+"-"+e1.getDepartment()+"-"+e1.getSalary()+"-"+e1.getYearsOfService()+"-"+e1.getParticularity());
+                +"-"+e1.getWeight()+"-"+e1.getDepartment()+"-"+e1.getSalary()+"-"+e1.getYearsOfService()+"-"+e1.getParticularity());
 
-        Employee e2 = new Employee("Stefanie", "Krug", "09.06.2000", 'f', 22, 0.0, "0" );
+        Employee e2 = new Employee("Maier", "Stefanie", "09.06.2000", 'f', 22, 62.2, "D24", "nothing" );
         System.out.println(e2.getName()+"-"+e2.getFirstname()+"-"+e2.getBirthdate()+"-"+e2.getGender()+"-"+e2.getAge()
                             +"-"+e2.getWeight()+"-"+e2.getDepartment()+"-"+e2.getSalary()+"-"+e2.getYearsOfService()+"-"+e2.getParticularity());
     }
